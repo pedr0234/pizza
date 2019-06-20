bd3e98a992d39ccb6ef98347312fd5b1d8b372fe
pedr0234 <51210418+pedr0234@users.noreply.github.com>
2019-06-19 23:32:03 -0500

    Add files via upload

---
+++ Pizza/js/pizza.js
@@ -0,0 +1,46 @@
+function fondo(color){
+    document.getElementById('rebanadaPizza').style="background:"+color;
+    document.getElementById('rebanadas1').style="background: transparent;";
+    document.getElementById('rebanadas2').style="background: transparent;";
+    document.getElementById('rebanadas3').style="background: transparent;";
+    document.getElementById('rebanadas4').style="background: transparent;";
+    document.getElementById('rebanadas5').style="background: transparent;";
+    document.getElementById('rebanadas6').style="background: transparent;";
+    document.getElementById('rebanadas7').style="background: transparent;";
+    document.getElementById('rebanadas8').style="background: transparent;";
+  }
+  function nombre(color){
+    document.getElementById('name').innerHTML="Su pedido es una: Pizza hawaiana";
+  }
+  function nombre2(color){
+    document.getElementById('name').innerHTML="Su pedido es una: Pizza Mexicana";
+  }
+  function nombre3(color){
+    document.getElementById('name').innerHTML="Su pedido es una: Pizza al pastor";
+  }
+  function nombre4(color){
+    document.getElementById('name').innerHTML="Su pedido es una: Pizza de peperonni";
+  }
+  function rebanada1(color){
+    document.getElementById('rebanadas1').style="background: gray;";
+  }
+  function rebanada2(color){
+    document.getElementById('rebanadas2').style="background: grey;";
+  }
+  function rebanada3(color){
+    document.getElementById('rebanadas3').style="background: grey;";
+  }
+  function rebanada4(color){
+    document.getElementById('rebanadas4').style="background: grey;";
+  }
+  function rebanada5(color){
+    document.getElementById('rebanadas5').style="background: grey;";
+  }
+  function rebanada6(color){
+    document.getElementById('rebanadas6').style="background: grey;";
+  }
+  function rebanada7(color){
+    document.getElementById('rebanadas7').style="background: grey;";
+  }function rebanada8(color){
+    document.getElementById('rebanadas8').style="background: grey;";
+  }
